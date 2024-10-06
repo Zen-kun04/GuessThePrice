fe8eee56c02d3e6469fde566648c049430133653
Baguette <baguette@france.com>
2024-10-06 17:23:29 +0200

    Added buttons for reducing and incrementing the price

--- src/main.py
+++ src/main.py
@@ -12,9 +12,7 @@ def main(page: ft.Page):
     page.add(ft.Row([ft.Text("Combien coûte l'objet suivant ?", size=36)], alignment=ft.MainAxisAlignment.CENTER))
     image = ft.Image(random_product["src"], border_radius=30, scale=0.8)
 
-    value = ft.TextField(value=0, border_radius=10, border_color="blue", keyboard_type=ft.KeyboardType.NUMBER, input_filter=ft.InputFilter(regex_string=r"^\d*$", allow=True, replacement_string=""), hint_text="Prix")
     def button_click(e: ft.ControlEvent):
-        print("Clicked")
         if int(value.value) != random_product["price"]:
             dialog = ft.AlertDialog(True, ft.Text("Mauvais prix !", size=24), ft.Text("Souhaitez vous réessayer avec le même objet ?"), [ ft.TextButton("Oui", on_click=lambda _: close_alert_ok(dialog)), ft.TextButton("Je passe", on_click=lambda _: close_alert_pass(dialog))])
             page.dialog = dialog
@@ -26,11 +24,15 @@ def main(page: ft.Page):
             dialog.open = True
             page.update()
 
+    value = ft.TextField(value=0, border_radius=10, border_color="blue", keyboard_type=ft.KeyboardType.NUMBER, input_filter=ft.InputFilter(regex_string=r"^\d*$", allow=True, replacement_string=""), hint_text="Prix", on_submit=button_click)
+    
+
     def replay(dialog: ft.AlertDialog):
         global random_product
         random_product = pick_random(data, random_product)
         image.src = random_product["src"]
         image.update()
+        value.value = 0
         dialog.open = False
         page.update()
 
@@ -43,12 +45,22 @@ def main(page: ft.Page):
         random_product = pick_random(data, random_product)
         image.src = random_product["src"]
         image.update()
+        value.value = 0
         dialog.open = False
         page.update()
 
     def input_change(e: ft.ControlEvent):
         value.value = e.data
         page.update()
+    
+    def decrease(_):
+        if int(value.value) > 0:
+            value.value = int(value.value) - 1
+            value.update()
+    
+    def increase(_):
+        value.value = int(value.value) + 1
+        value.update()
 
     value.on_change=input_change
 
@@ -58,11 +70,17 @@ def main(page: ft.Page):
                 image,
                 ft.Column(
                     [
-                        value,
+                        ft.Row(
+                            [
+                                ft.IconButton(ft.icons.REMOVE, on_click=decrease),
+                                value,
+                                ft.IconButton(ft.icons.ADD, on_click=increase),
+                            ],
+                        ),
                         ft.TextButton("J'achète", scale=1.2, style=ft.ButtonStyle(side=ft.BorderSide(color="blue", width=1), shape=ft.RoundedRectangleBorder(radius=5)), on_click=button_click)
                     ],
                     
-                )
+                ),
             ],
             alignment=ft.MainAxisAlignment.CENTER,
             ))
